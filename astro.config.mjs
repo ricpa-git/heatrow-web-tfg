76fe06ed4aa0433f3aa7050f84024dc35bf2666e
richipa <ricardoperezaragoneses@gmail.com>
2026-04-07 15:53:57 +0200

    Add Netlify adapter and fix npm vulnerabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   integrations: [react(), tailwind()],
   vite: {
     resolve: {

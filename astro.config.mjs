8f66b988477f19c8ac7a0a7bb8696725f2a2bef8
richipa <ricardoperezaragoneses@gmail.com>
2026-04-10 09:40:38 +0200

    Update netlify adapter to latest version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  // @ts-ignore
   adapter: netlify(),
   integrations: [react(), tailwind()],
   vite: {

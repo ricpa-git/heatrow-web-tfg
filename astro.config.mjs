def9f4bbfa1a45bbeb0cd088b7c6452a817a854d
richipa <ricardoperezaragoneses@gmail.com>
2026-04-10 12:36:44 +0200

    Remove prerender false and use static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 export default defineConfig({
   output: 'static',
   integrations: [react(), tailwind()],
